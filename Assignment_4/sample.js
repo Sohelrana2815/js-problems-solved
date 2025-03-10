e90cf63a5b2a28f391c71a3f3ef126c607775f82
Sohelrana2815 <sohel152302@gmail.com>
2025-03-10 07:15:54 +0600

    solved calculate money problem

--- Assignment_4/sample.js
+++ Assignment_4/sample.js
@@ -1,8 +1,16 @@
+// No.1 solution
 function calculateMoney(ticketSale) {
-
-
+  if (ticketSale < 0 || ticketSale === 0) {
+    return "Invalid Number";
+  }
   const securityGuardExpense = 500;
   const lunchExpense = 8 * 50;
+  const totalExpenses = securityGuardExpense + lunchExpense;
+  const ticketSalesRevenue = 120 * ticketSale;
 
-  
+  const remainingMoney = ticketSalesRevenue - totalExpenses;
+
+  return remainingMoney;
 }
+
+// console.log(calculateMoney(0));
