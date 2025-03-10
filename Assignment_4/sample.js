70ca20779f9da450778da3da021a2669dda596dd
Sohelrana2815 <sohel152302@gmail.com>
2025-03-10 07:44:00 +0600

    start solving 3 no. problem virus in my array

--- Assignment_4/sample.js
+++ Assignment_4/sample.js
@@ -1,45 +1,51 @@
-// No.1 solution
-function calculateMoney(ticketSale) {
-  if (ticketSale < 0 || ticketSale === 0) {
-    return "Invalid Number";
-  }
-  const securityGuardExpense = 500;
-  const lunchExpense = 8 * 50;
-  const totalExpenses = securityGuardExpense + lunchExpense;
-  const ticketSalesRevenue = 120 * ticketSale;
+// // No.1 solution
+// function calculateMoney(ticketSale) {
+//   if (ticketSale < 0 || ticketSale === 0) {
+//     return "Invalid Number";
+//   }
+//   const securityGuardExpense = 500;
+//   const lunchExpense = 8 * 50;
+//   const totalExpenses = securityGuardExpense + lunchExpense;
+//   const ticketSalesRevenue = 120 * ticketSale;
 
-  const remainingMoney = ticketSalesRevenue - totalExpenses;
+//   const remainingMoney = ticketSalesRevenue - totalExpenses;
 
-  return remainingMoney;
-}
+//   return remainingMoney;
+// }
 
-// console.log(calculateMoney(0));
+// // console.log(calculateMoney(0));
 
-// No.2 solution
+// // No.2 solution
 
-function checkName(name) {
-  if (typeof name !== "string" || name === "") {
-    return "Name should string.";
-  }
-  const goodNameCharacters = ["a", "e", "i", "o", "u", "w", "y"];
+// function checkName(name) {
+//   if (typeof name !== "string" || name === "") {
+//     return "Name should string.";
+//   }
+//   const goodNameCharacters = ["a", "e", "i", "o", "u", "w", "y"];
 
-  const lastCharacter =
-    name.length > 0 ? name[name.length - 1].toLocaleLowerCase() : "";
+//   const lastCharacter =
+//     name.length > 0 ? name[name.length - 1].toLocaleLowerCase() : "";
 
-  let isGoodName = false;
+//   let isGoodName = false;
 
-  for (let i = 0; i < goodNameCharacters.length; i++) {
-    if (goodNameCharacters[i] === lastCharacter) {
-      isGoodName = true;
-      break;
-    }
-  }
+//   for (let i = 0; i < goodNameCharacters.length; i++) {
+//     if (goodNameCharacters[i] === lastCharacter) {
+//       isGoodName = true;
+//       break;
+//     }
+//   }
 
-  if (isGoodName) {
-    return "GoodName";
-  } else {
-    return "Bad Name";
-  }
-}
+//   if (isGoodName) {
+//     return "GoodName";
+//   } else {
+//     return "Bad Name";
+//   }
+// }
 
-console.log(checkName(""));
+// console.log(checkName(""));
+
+
+
+
+// No.3 solution
+
