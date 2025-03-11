2f858f1a2c5be3f6a21af4cebf15ea4f27a51444
Sohelrana2815 <sohel152302@gmail.com>
2025-03-11 21:06:00 +0600

    done

---
+++ Assignment_4/forEach.js
@@ -0,0 +1,7 @@
+const names = ["moon", "sun", "john", "max"];
+
+names.forEach((name) => {
+  console.log(name);
+});
+
+// No return for forEach method
--- Assignment_4/task1.js
+++ Assignment_4/task1.js
@@ -1,2 +1,17 @@
-let args = process.argv.slice(2);
-console.log(args);
+const a = process.argv.slice(2);
+if (!a.length) console.log("");
+else if (a.length == 1) {
+  console.log(a[0]);
+} else {
+  const f = a[0];
+  for (let l = f.length; l; l--)
+    for (let i = 0; i <= f.length - l; i++) {
+      const s = f.substr(i, l);
+      if (a.every((x) => x.includes(s))) {
+        console.log(s);
+        process.exit();
+      }
+    }
+
+  console.log("");
+}
